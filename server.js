7693bb4477cb26ce96cd7849a6a004149e7bd08a
Mike Colbert <mcolbert1@live.maryville.edu>
2021-04-14 14:14:58 -0500

    update package name

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/<name-of-app-in-package.json>"));
+app.use(express.static(__dirname + "/dist/swdv691-ui"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname + "/dist/<name-of-appp-in-package.json>/index.html")
-  );
+  res.sendFile(path.join(__dirname + "/dist/swdv691-ui/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
