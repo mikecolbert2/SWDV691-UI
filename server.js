ac0f03c9eb38c02fc8d4f80639e972b90ffe04fd
Mike Colbert <mcolbert1@live.maryville.edu>
2021-04-14 15:16:17 -0500

    troubleshooting heroku deploy

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/timedetective-ui"));
+app.use(express.static(__dirname + "/dist/SWDV691-UI"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/timedetective-ui/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/SWDV691-UI/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
