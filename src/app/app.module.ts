f28961cb8f92ee653f1b8d3340abfceb5c369faf
Mike Colbert <mcolbert1@live.maryville.edu>
2021-04-15 19:44:49 -0500

    welcome & about pages + basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/layout/header/header.component';
+import { FooterComponent } from './component/layout/footer/footer.component';
+import { AboutComponent } from './component/pages/about/about.component';
+import { WelcomeComponent } from './component/pages/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
